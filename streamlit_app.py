e124c04fba9bba7556e250183d41698b2bf9e446
Abdalla Gamal <52201436+AbdallaGamal28@users.noreply.github.com>
2024-02-07 19:49:23 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,6 @@ with st.form('my_form'):
   text = st.text_area('Enter text:', 'Give me a use case and ask for test cases')
   submitted = st.form_submit_button('Submit')
   if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='4') 
+    st.warning('Please enter your OpenAI API key!', icon='') 
   if submitted and openai_api_key.startswith('sk-'):
     generate_response(text)
