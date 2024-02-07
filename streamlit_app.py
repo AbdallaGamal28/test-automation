7407aee4e50900421fea98bb01920cc4716b9531
Abdalla Gamal <52201436+AbdallaGamal28@users.noreply.github.com>
2024-02-07 19:44:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ st.title("Quickstart Test Automation App")
 openai_api _key = st.sidebar.text_input("OpenAI API Key")
 
 def generate_response(input_text):
-  11m = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-  st.info(11m(input_text))
+  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+  st.info(llm(input_text))
 
 with st.form('my_form'):
   text = st.text_area('Enter text:', 'Give me a use case and ask for test cases')
