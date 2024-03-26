c1dbd15e0899eede1530f50db86bf428457b235d
Abdalla Gamal <52201436+AbdallaGamal28@users.noreply.github.com>
2024-03-26 15:39:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
   st.info(llm(input_text))
 
-with st.form('my_form'):
+with st.form('my_form1'):
   text = st.text_area('Enter text:', 'Give me a use case and ask for test cases')
   submitted = st.form_submit_button('Submit')
   if not openai_api_key.startswith('sk-'):
@@ -22,7 +22,7 @@ from langchain.llms import OpenAI
 
 st.title("Create a test automation script out of it ")
 
-with st.form('my_form'):
+with st.form('my_form2'):
   text = st.text_area('Enter text:', 'type the use case and ask for test script')
   submitted = st.form_submit_button('Submit')
   if not openai_api_key.startswith('sk-'):
