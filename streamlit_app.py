84798a640760a33de587a551749e080e4b55d161
Abdalla Gamal <52201436+AbdallaGamal28@users.noreply.github.com>
2024-03-26 15:46:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,14 @@ def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
   st.info(llm(input_text))
 
+with st.form('my_form0'):
+  text = st.text_area('Enter text:', 'Give me a user story to analyze it')
+  submitted = st.form_submit_button('Submit')
+  if not openai_api_key.startswith('sk-'):
+    st.warning('Please enter your OpenAI API key!', icon='⚠️') 
+  if submitted and openai_api_key.startswith('sk-'):
+    generate_response(text)
+
 with st.form('my_form1'):
   text = st.text_area('Enter text:', 'Give me a use case and ask for test cases')
   submitted = st.form_submit_button('Submit')
@@ -18,22 +26,9 @@ with st.form('my_form1'):
     generate_response(text)
 
 
-st.title("Create a test automation  ")
+st.title("Create a test automation for the above scenarios /please copy the TCs you need to automate/")
 
 with st.form('my_form2'):
-  text = st.text_area('Enter text:', 'type the use case and ask for test script')
-  submitted = st.form_submit_button('Submit')
-  if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠️') 
-  if submitted and openai_api_key.startswith('sk-'):
-    generate_response(text)
-
-import streamlit as st 
-from langchain.llms import OpenAI
-
-st.title("Create a test automation script out of it ")
-
-with st.form('my_form3'):
   text = st.text_area('Enter text:', 'type the use case and ask for test script')
   submitted = st.form_submit_button('Submit')
   if not openai_api_key.startswith('sk-'):
